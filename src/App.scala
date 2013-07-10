5c471e66571dd7bc2abe42bc23d8b8cbc987b22f
David Riccitelli <david@insideout.io>
2013-07-10 21:32:50 +0300

    code cleaning

--- src/App.scala
+++ src/App.scala
@@ -1,25 +1,25 @@
 import java.io.PrintWriter
 import java.net.URL
 import scala.xml.{Elem, Node, XML}
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.mutable.Map
+import scala.collection._
 
 object App {
 
   def main(args: Array[String]) {
 
-    var nc: Long  = 0
-
     val url: URL       = new URL("http://overpass-api.de/api/xapi_meta?*[amenity%3Dcharging_station]")
-    lazy val xml: Elem = XML.load(url)
+    val nodes          = XML.load(url) \ "node"
 
-    val fields         = ArrayBuffer[String]("id", "version", "timestamp", "uid", "user", "changeset", "lat", "lon")
+    val fields         = mutable.ArrayBuffer[String]("id", "version", "timestamp", "uid", "user", "changeset", "lat", "lon")
     val ignore         = Array[String]("what_kind_of_plug", "addr:city", "addr:country", "addr:housenumber", "refID",
                          "addr:postcode", "addr:street", "addr:suburb", "fixme", "FIXME", "addr:housename",
-                         "addr:floor", "addr:state")
+                         "addr:floor", "addr:state", "fuel:electricity", "note", "petrol",
+                         "rwetempcharging_station:plug", "note:de", "description", "description:de", "start_date",
+                         "operator:en", "hamburgenergie", "vattenfall", "survey:date", "wheelchair", "places", "price",
+                         "phone", "shop", "network", "accuracy", "layer", "count", "date", "building:levels")
     lazy val writer    = new PrintWriter("charging-stations.tsv")
 
-    def nodeMap(n: Node) = Map(
+    def nodeMap(n: Node) = mutable.Map(
       "id"        -> (n \ "@id").text,
       "version"   -> (n \ "@version").text,
       "timestamp" -> (n \ "@timestamp").text,
@@ -30,11 +30,10 @@ object App {
       "lon"       -> (n \ "@lon").text
     )
 
-    def printMap(map: Map[String, String]) = fields foreach { f: String => writer.print(map.getOrElse(f, "") + "\t") }
-    def printFields                        = fields foreach { f: String => writer.print(f + "\t") }
+    def printMap(map: Map[String, String]) = fields foreach { f => writer.print(map.getOrElse(f, "") + "\t") }
+    def printFields()                      = fields foreach { f => writer.print(f + "\t") }
 
-    (xml \ "node") foreach { n: Node =>
-      nc += 1
+    nodes foreach { n: Node =>
       val nm = nodeMap(n)
       (n \ "tag") foreach { t: Node =>
         val k = (t \ "@k").text
@@ -47,10 +46,10 @@ object App {
       writer.print("\n")
     }
 
-    printFields
+    printFields()
     writer.close()
 
-    println("nodes : " + nc)
+    println("nodes : " + nodes.length)
     println("fields: " + fields.length)
   }
 
