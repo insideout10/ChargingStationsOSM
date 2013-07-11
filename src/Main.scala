2d41af43ff3e8819ab600e3b0ae49baf92d36899
David Riccitelli <david@insideout.io>
2013-07-11 10:12:18 +0300

    remove mutable collections

--- src/Main.scala
+++ src/Main.scala
@@ -1,6 +1,6 @@
 import java.io.PrintWriter
 import java.net.URL
-import scala.xml.{Elem, Node, XML}
+import scala.xml.{Node, XML}
 import scala.collection._
 
 object Main extends App {
@@ -8,8 +8,8 @@ object Main extends App {
   val url: URL       = new URL("http://overpass-api.de/api/xapi_meta?*[amenity%3Dcharging_station]")
   val nodes          = XML.load(url) \ "node"
 
-  val fields         = mutable.ArrayBuffer[String]("id", "version", "timestamp", "uid", "user", "changeset", "lat", "lon")
-  val ignore         = Array[String]("what_kind_of_plug", "addr:city", "addr:country", "addr:housenumber", "refID",
+  val initFields     = Seq("id", "version", "timestamp", "uid", "user", "changeset", "lat", "lon")
+  val ignoreFields   = Set("what_kind_of_plug", "addr:city", "addr:country", "addr:housenumber", "refID",
                        "addr:postcode", "addr:street", "addr:suburb", "fixme", "FIXME", "addr:housename",
                        "addr:floor", "addr:state", "fuel:electricity", "note", "petrol",
                        "rwetempcharging_station:plug", "note:de", "description", "description:de", "start_date",
@@ -17,7 +17,7 @@ object Main extends App {
                        "phone", "shop", "network", "accuracy", "layer", "count", "date", "building:levels")
   lazy val writer    = new PrintWriter("charging-stations.tsv")
 
-  def nodeMap(n: Node) = mutable.Map(
+  def nodeMap(n: Node) = Map(
     "id"        -> (n \ "@id").text,
     "version"   -> (n \ "@version").text,
     "timestamp" -> (n \ "@timestamp").text,
@@ -28,26 +28,24 @@ object Main extends App {
     "lon"       -> (n \ "@lon").text
   )
 
-  def printMap(map: Map[String, String]) = fields foreach { f => writer.print(map.getOrElse(f, "") + "\t") }
-  def printFields()                      = fields foreach { f => writer.print(f + "\t") }
+  def printMap(map: Map[String, String], fs: Seq[String]) = fs foreach { f => writer.print(map.getOrElse(f, "") + "\t") }
+  def printFields(fs: Seq[String])                        = fs foreach { f => writer.print(f + "\t") }
 
-  nodes foreach { n: Node =>
-    val nm = nodeMap(n)
-    (n \ "tag") foreach { t: Node =>
-      val k = (t \ "@k").text
-      val v = (t \ "@v").text
-
-      nm += (k -> v)
-      if (!ignore.contains(k) && !fields.contains(k)) fields += k
+  val fields = nodes.foldLeft(initFields) { (fields, n) =>
+    val nm = (n \ "tag").foldLeft(nodeMap(n)) { (nm, t) =>
+      nm + ((t \ "@k").text -> (t \ "@v").text)
     }
-    printMap(nm)
-    writer.print("\n")
+
+    val fs = fields ++ ((nm.keySet -- ignoreFields) -- fields)
+    printMap(nm, fs); writer.print("\n")
+
+    fs
   }
 
-  printFields()
+  printFields(fields)
   writer.close()
 
   println("nodes : " + nodes.length)
-  println("fields: " + fields.length)
+  println("fields: " + fields.size)
 
 }
